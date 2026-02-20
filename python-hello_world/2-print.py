633899352f72cec6daeb244e812b9fd8ee4a998e
mweene-wq <g.mweene@alustudent.com>
2026-02-20 14:48:28 -0800

    new line

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 print("\"Programming is like building a multilingual puzzle")
-=======
-print("\"Programming is like building a multilingual puzzle")
->>>>>>> a9cfc0ca8a91cdbee50ea94b7f3b39dfc3cc814b
